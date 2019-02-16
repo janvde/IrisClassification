642c5efce7de0d67853740ca7baf418a76c4c4b0
Jan van den Ende <jan@getroadmap.com>
2019-02-16 16:50:57 +0100

    cleanup and comments

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const names = ['sepalLength', 'sepalWidth', 'petalLength', 'petalWidth', 'type']
 
 const testRatio = 0.8;
 
-let iris = [];
-
 //read data
 csv({noheader: true, headers: names})
     .fromFile(csvFilePath)
-    .then(function (jsonObj) {
-        iris = jsonObj
-    })
-    .finally(function () {
-        //data preparation
+    .then(function (iris) {
+        //shuffle array, since the dataset is ordered.
         iris = shuffleArray(iris);
+        //normalize the data by converting strings to numbers
         iris = normalize(iris);
 
         //split into training and test set
@@ -33,33 +29,46 @@ csv({noheader: true, headers: names})
 
         //train the model with the training set
         var knn = new KNN(trainingSetData, trainingSetPrediction);
-        console.log(`Training with data set size = ${trainingSetData.length}`)
+        console.log(`Training with data set size = ${trainingSetData.length}`);
 
         //test the model with the test set
         const result = knn.predict(testSetData);
         const predictionError = error(result, testSetPrediction);
         console.log(`Testing with test set size = ${testSetData.length} and number of errors = ${predictionError}`);
-
-
     });
 
 
+/**
+ * calculate error as amount of errors between expected and predicted
+ * @param predicted array
+ * @param expected array
+ * @return number of errors
+ */
 function error(predicted, expected) {
-    let misclassifications = 0;
-    for (var index = 0; index < predicted.length; index++) {
-        if (predicted[index] !== expected[index]) {
-            misclassifications++;
+    let errors = 0;
+    predicted.forEach(function (prediction, index) {
+        if (prediction !== expected[index]) {
+            errors++;
         }
-    }
-    return misclassifications;
+    });
+
+    return errors;
 }
 
+/**
+ * get data, without label
+ * @param 2-dimensional array of numbers
+ */
 function getData(array) {
     return array.map(function (item) {
-        return Object.values(item).slice(0,4);
+        return Object.values(item).slice(0, 4);
     })
 }
 
+/**
+ * get label without data
+ *
+ * */
 function getPrediction(array) {
     return array.map(function (item) {
         return item.type
@@ -67,6 +76,11 @@ function getPrediction(array) {
 
 }
 
+/**
+ * convert string numbers to floats and convert flower types to numbers 1-3
+ * @param array
+ * @return array of normalized data
+ */
 function normalize(array) {
     return array.map(function (item) {
         let type = 0;
@@ -84,11 +98,13 @@ function normalize(array) {
     });
 }
 
-
+/**
+ * randomize array order
+ */
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
